2004168e874d22de011cfb885e51b1dc10853cb9
OyvindFlemmen <flemmenoy2@hotmail.com>
2026-04-06 17:16:02 +0300

    Sources page

--- main-sources.php
+++ main-sources.php
@@ -1,12 +1,86 @@
 <?php 
-$bg = "../images/....jpg";
+$bg = "../images/main_sources.jpg";
 include 'includes/header.php'; 
 ?>
 
 <!-- Main content -->
-<main class="col-lg-10 p-4" style="--bg-image: url('<?php echo $bg; ?>')">
-    <h1>Main Sources</h1>
-    <p>Inlcude some descriptions of each book/work cited and a picture. Maybe a link to amazon web store?</p>
+<main class="col-lg-10 p-4 left-centered" style="--bg-image: url('<?php echo $bg; ?>')">
+    <h1 class="page-title">Main Sources</h1>
+
+    <div class="d-flex gap-5 align-items-center mb-5">
+        <a href="https://www.amazon.com/Principles-Digital-Audio-Sixth-Video/dp/0071663460">
+            <img src="images/pohlmann.jpg" class="source-image">
+        </a>
+        <div>
+            <h5><a class="link-light" href="https://www.amazon.com/Principles-Digital-Audio-Sixth-Video/dp/0071663460">Principles of Digital Audio, Sixth Edition</a></h5>
+            <p>
+                Written by industry pioneer and Audio Engineering Society Fellow Ken C. Pohlmann, Principles of Digital Audio, Sixth Edition, 
+                describes the technologies behind today's audio equipment in a clear, practical style. Covering basic theory to the latest technological advancements, 
+                the book explains how to apply digital conversion, processing, compression, storage, streaming, and transmission concepts. 
+            </p>
+        </div>
+    </div>
+
+
+    <div class="d-flex gap-5 align-items-center mb-5">
+        <a href="https://www.amazon.com/Psychoacoustics-Models-Springer-Information-Sciences/dp/3540231595">
+            <img src="images/psychoacoustics.jpg" class="source-image">
+        </a>
+        <div>
+            <h5><a class="link-light" href="https://www.amazon.com/Psychoacoustics-Models-Springer-Information-Sciences/dp/3540231595">Psychoacoustics: Facts and Models</a></h5>
+            <p>
+                Psychoacoustics – Facts and Models offers a unique, comprehensive summary of information describing the processing of sound by the human hearing system. 
+                It includes quantitative relations between sound stimuli and auditory perception in terms of hearing sensations, for which quantitative models are given, 
+                as well as an unequalled collection of data on the human hearing system as a receiver of acoustic information. In addition, many examples of the practical application 
+                of the results of basic research in fields such as noise control, audiology, or sound quality engineering are detailed.
+            </p>
+        </div>
+    </div>
+
+    <div class="d-flex gap-5 align-items-center mb-5">
+        <a href="https://www.amazon.com/Introduction-Digital-Communications-Grami-Ph-D/dp/0124076823">
+            <img src="images/digital_communications.jpg" class="source-image">
+        </a>
+        <div>
+            <h5><a class="link-light" href="https://www.amazon.com/Introduction-Digital-Communications-Grami-Ph-D/dp/0124076823">Introduction to Digital Communications</a></h5>
+            <p>
+                Introduction to Digital Communications explores the basic principles in the analysis and design of digital communication systems, 
+                including design objectives, constraints and trade-offs. After portraying the big picture and laying the background material, 
+                this book lucidly progresses to a comprehensive and detailed discussion of all critical elements and key functions in digital communications.
+            </p>
+        </div>
+    </div>
+
+    <div class="d-flex gap-5 align-items-center mb-5">
+        <a href="https://www.amazon.com/Fundamentals-Acoustics-Lawrence-Kinsler/dp/0471029335">
+            <img src="images/acoustics.jpg" class="source-image">
+        </a>
+        <div>
+            <h5><a class="link-light" href="https://www.amazon.com/Fundamentals-Acoustics-Lawrence-Kinsler/dp/0471029335">Fundamentals of Acoustics</a></h5>
+            <p>
+                A clear treatment of the fundamental principles underlying the generation, transmission, and reception of acoustic waves and their application to numerous fields. 
+                Analyzes the various types of vibration of solid bodies and the propagation of sound waves through fluid media. The third edition features discussions of antiresonance concert hall acoustics, 
+                detection theory, canonical equations, normal mode propagation in the ocean, and environmental acoustics. Material on absorption, hearing, architectural acoustics, 
+                and underwater sound has been expanded and updated.
+            </p>
+        </div>
+    </div>
+
+    <div class="d-flex gap-5 align-items-center mb-5">
+        <a href="https://www.amazon.com/Discrete-Time-Signal-Processing-3rd-Prentice-Hall/dp/0131988425">
+            <img src="images/signal_processing.jpg" class="source-image">
+        </a>
+        <div>
+            <h5><a class="link-light" href="https://www.amazon.com/Discrete-Time-Signal-Processing-3rd-Prentice-Hall/dp/0131988425">Discrete-Time Signal Processing</a></h5>
+            <p>
+                Discrete-Time Signal Processing, Third Edition is the definitive, authoritative text on DSP – ideal for those with introductory-level knowledge of signals and systems. 
+                Written by prominent DSP pioneers, it provides thorough treatment of the fundamental theorems and properties of discrete-time linear systems, filtering, sampling, 
+                and discrete-time Fourier Analysis. By focusing on the general and universal concepts in discrete-time signal processing, it remains vital and relevant to the new challenges 
+                arising in the field.
+            </p>
+        </div>
+    </div>
+
 </main>
 
 <?php include 'includes/footer.php'; ?>
