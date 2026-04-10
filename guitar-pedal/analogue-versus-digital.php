71b77ffb42f44bb22bc340a13aa0afb0344e18a9
OyvindFlemmen <flemmenoy2@hotmail.com>
2026-04-10 14:52:42 +0300

    Finished the guitar pedal pages

--- guitar-pedal/analogue-versus-digital.php
+++ guitar-pedal/analogue-versus-digital.php
@@ -1,30 +1,94 @@
-<?php 
+<?php
 $bg = "../images/guitar_pedal.jpg";
-include '../includes/header.php'; 
+include '../includes/header.php';
 ?>
 
 <!-- Main content -->
 <main class="col-lg-10 p-4 left-centered" style="--bg-image: url('<?php echo $bg; ?>')">
-    <h1 class="page-title">Analogue versus digital</h1>
+        <h1 class="page-title">Analogue versus digital</h1>
 
-    <h4>Analogue Pedals</h4>
-    <p>
-            Historically, guitar pedals were mostly designed as fully analogue devices. These are usually built from simple components, providing a high effects quality at a relatively low cost. 
-            However, they are also rather rigid in their design. Due to the processing being solely hardware-based, they provide limited control over the effects parameters, 
-            and are usually not suited for implementing long or flexible time-based effects such as delay. 
-            The limited scope of a typical analogue pedal also means that musicians who wish to experiment with different effects often need to purchase multiple devices, 
-            significantly increasing the overall cost.
-    </p>
+        <h4>Analogue Pedals</h4>
+        <p>
+                Historically, guitar pedals were mostly designed as fully analogue devices. These are usually built from simple components, providing a high effects quality at a relatively low cost.
+                However, they are also rather rigid in their design. Due to the processing being solely hardware-based, they provide limited control over the effects parameters,
+                and are usually not suited for implementing long or flexible time-based effects such as delay.
+                The limited scope of a typical analogue pedal also means that musicians who wish to experiment with different effects often need to purchase multiple devices,
+                significantly increasing the overall cost.
+        </p>
 
-    <h4>Digital Pedals</h4>
-    <p>
-            Digital guitar multi-effects pedals therefore offer greater flexibility at a lower relative cost, combining several different effects into a single device, 
-            including effects that are impractical to implement in hardware. Additionally, changing the set of effects or the parameters of digitally processed signals is usually a matter of code changes in the firmware, 
-            rather than changes to the existing hardware, as with analogue systems. This simplifies the development of a functional prototype, and allows for additional effects customization. 
-            However, these devices rely on analogue-to-digital, and digital-to-analogue conversion to process the signal, and thus, 
-            the audio quality is limited by the sampling rate and resolution. Processing the effects reliably, especially those that rely on complex mathematical operations, 
-            may also introduce considerable latency between the input and output. 
-    </p>
+        <div class="row mb-5">
+
+                <div class="col-md-6 d-flex gap-3 align-items-center">
+                        <a href="https://www.boss.info/us/products/ds-1/">
+                                <img src="/images/ds1.png" class="pedal-image">
+                        </a>
+                        <div>
+                                <h5><a class="link-light" href="https://www.boss.info/us/products/ds-1/">Boss DS-11</a></h5>
+                                <p>
+                                        The DS-1 Distortion is a true icon in the world of guitar effects. Introduced in 1978, BOSS’s first distortion pedal defined a bold new sound,
+                                        delivering hard-edged attack and smooth sustain that’s been a staple of players for generations. The DS-1 is the top-selling BOSS compact pedal ever,
+                                        and its original, unchanged design continues to inspire the creation of great music everywhere.
+                                </p>
+                        </div>
+                </div>
+
+                <div class="col-md-6 d-flex gap-3 align-items-center">
+                        <a href="https://www.jimdunlop.com/mxr-phase-90/">
+                                <img src="/images/mxr_phase_90.png" class="pedal-image">
+                        </a>
+                        <div>
+                                <h5><a class="link-light" href="https://www.jimdunlop.com/mxr-phase-90/">MXR Phase 90</a></h5>
+                                <p>
+                                        For more than four decades, the MXR Phase 90 has been a mainstay on the pedal boards of millions of players around the globe.
+                                        This little orange box went on to become the sole icon of its effect category, and countless legendary riffs have benefitted from the sonic qualities of
+                                        this pedal. No matter the genre or instrument, the Phase 90 has been there through it all to add its distinctly lush voice to a musician's tone palette.
+                                        With the twist of the Rate knob, you can take the Phase 90's warm modulation from subtle, spatial shimmer to all-out high velocity swooshing.
+                                </p>
+                        </div>
+                </div>
+        </div>
+
+        <h4>Digital Pedals</h4>
+        <p>
+                Digital guitar multi-effects pedals therefore offer greater flexibility at a lower relative cost, combining several different effects into a single device,
+                including effects that are impractical to implement in hardware. Additionally, changing the set of effects or the parameters of digitally processed signals is usually a matter of code changes in the firmware,
+                rather than changes to the existing hardware, as with analogue systems. This simplifies the development of a functional prototype, and allows for additional effects customization.
+                However, these devices rely on analogue-to-digital, and digital-to-analogue conversion to process the signal, and thus,
+                the audio quality is limited by the sampling rate and resolution. Processing the effects reliably, especially those that rely on complex mathematical operations,
+                may also introduce considerable latency between the input and output.
+        </p>
+
+
+        <div class="row mb-5">
+                <div class="col-md-6 d-flex gap-3 align-items-center">
+
+                        <a href="https://line6.com/helix/helix-lt.html">
+                                <img src="/images/helix.png" class="pedal-image">
+                        </a>
+                        <div>
+                                <h5><a class="link-light" href="https://line6.com/helix/helix-lt.html">Line 6 Helix</a></h5>
+                                <p>
+                                        The streamlined Helix® LT boasts the same dual-SHARC® processing and professional-quality HX® Modeling found in the flagship Helix Floor and
+                                        Helix Rack amp and effects processors—outperforming all other products in its class. Features include a player-friendly interface with a large color LCD,
+                                        capacitive-touch footswitches for easy editing, an onboard expression pedal, analog and digital I/O, and a USB audio interface.
+                                </p>
+                        </div>
+                </div>
+
+                <div class="col-md-6 d-flex gap-3 align-items-center">
+
+                        <a href="https://www.boss.info/us/products/dd-3/">
+                                <img src="/images/dd3.png" class="pedal-image">
+                        </a>
+                        <div>
+                                <h5><a class="link-light" href="https://www.boss.info/us/products/dd-3/">Boss DD-3</a></h5>
+                                <p>
+                                        This compact pedal provides a digital delay effect with outstanding quality equivalent to that of a dedicated rackmount delay unit, all with simple stompbox-style control.
+                                        Provides 3 delay time modes and a Delay Time control for quick adjustment of exact delay time between 12.5ms – 800ms.
+                                </p>
+                        </div>
+                </div>
+        </div>
 </main>
 
 <?php include '../includes/footer.php'; ?>
--- guitar-pedal/explanation.php
+++ guitar-pedal/explanation.php
@@ -4,11 +4,72 @@ include '../includes/header.php';
 ?>
 
 <!-- Main content -->
-<main class="col-lg-10 p-4 hero-section" style="--bg-image: url('<?php echo $bg; ?>')">
+<main class="col-lg-10 p-4 left-centered" style="--bg-image: url('<?php echo $bg; ?>')">
     <h1 class="page-title">Explanation of different effects</h1>
     <p>
-        something something
+        Below, I've listed some of the more common guitar effects that are commonly found in guitar effect pedals. This is by no means an exhaustive list, 
+        and is instead meant as a broad overview to introduce the basics. Under the explanation of each effect, I've also included an audio players with examples of what each effect may sound like.
+        I did not provide a comprehensive explanation of the effects in my bachelor's thesis documement, so the following explanations are mostly taken from ChatGPT.
     </p>
+
+    <h4>Distortion</h4>
+    <p>
+        Distortion is perhaps the simplest effect to explain. It alters a guitar signal by clipping the waveform, 
+        meaning the peaks of the signal are cut off when they exceed a certain level. This introduces additional harmonic content, making the sound richer, more aggressive, and sustained. 
+        Depending on how strongly the signal is clipped, distortion can range from mild overdrive to heavy, saturated tones used in rock and metal.
+    </p>
+    <div class="d-flex gap-3 align-items-center mb-4">
+        <div class="audio-example">
+            <h6>Example:</h6>
+            <a class="link-light" href="https://www.youtube.com/watch?v=xGYmX7yVqEg/">VILT - Enslaved</a>
+        </div>
+        <audio controls src="/audio/distortion.mp3"></audio>
+    </div>
+
+    <h4>Delay</h4>
+    <p>
+        A delay effect works by recording the input signal and playing it back after a short time, creating an echo. 
+        The delayed signal can be repeated multiple times with decreasing amplitude using feedback, producing a series of echoes. By adjusting the delay time and feedback level, 
+        the effect can range from a subtle sense of space to pronounced rhythmic repeats.
+    </p>
+    <div class="d-flex gap-3 align-items-center mb-4">
+        <div class="audio-example">
+            <h6>Example:</h6>
+            <a class="link-light" href="https://www.youtube.com/watch?v=9erLsEHAZRI">Guns'n Roses - Welcome to the Jungle</a>
+        </div>
+        <audio controls src="/audio/delay.mp3"></audio>
+    </div>
+
+
+    <h4>Tremolo</h4>
+    <p>
+        A tremolo effect modulates the amplitude (volume) of the signal over time, causing it to periodically get louder and quieter. 
+        This modulation is typically controlled by a low-frequency oscillator (LFO), which determines the rate (speed) and depth (intensity) of the effect. At subtle settings, 
+        tremolo adds a gentle pulsing character, while at higher depths and speeds it produces a pronounced, rhythmic chopping sound.
+    </p>
+    <div class="d-flex gap-3 align-items-center mb-4">
+        <div class="audio-example">
+            <h6>Example:</h6>
+            <a class="link-light" href="https://www.youtube.com/watch?v=qgDrpWWxuto">Nancy Sinatra - Bang Bang</a>
+        </div>
+        <audio controls src="/audio/tremolo.mp3"></audio>
+    </div>
+
+    <h4>Chorus</h4>
+    <p>
+        A chorus effect works by mixing the original signal with one or more slightly delayed and pitch-modulated copies of itself. 
+        These small variations in timing and pitch, typically controlled by a low-frequency oscillator (LFO), create the illusion of multiple instruments playing the same part simultaneously. 
+        The result is a thicker, wider, and more spacious sound, often described as “shimmering” or “lush.”
+    </p>
+    <div class="d-flex gap-3 align-items-center mb-4">
+        <div class="audio-example">
+            <h6>Example:</h6>
+            <a class="link-light" href="https://www.youtube.com/watch?v=LwfMqgpZe5U">Guns N' Roses - Paradise City</a>
+        </div>
+        <audio controls src="/audio/chorus.mp3"></audio>
+    </div>
+
+
 </main>
 
 <?php include '../includes/footer.php'; ?>
