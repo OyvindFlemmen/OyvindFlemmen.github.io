f85987a4b40f3dd613351f3906b7b54f52b6a07b
OyvindFlemmen <flemmenoy2@hotmail.com>
2026-04-12 20:58:23 +0300

    Finished effect sim

--- effect-sim.php
+++ effect-sim.php
@@ -7,16 +7,16 @@ include 'includes/header.php';
 <main class="col-lg-10 p-4 hero-section" style="--bg-image: url('<?php echo $bg; ?>')">
     <h1 class="page-title">Effect Simulator</h1>
     <div class="page-text">
-        <div class="container-fluid bg-dark text-light">
+        <div class="container-fluid bg-dark text-light pb-4">
             <div class="row h-100 mb-4">
 
                 <div class="border effect-panel">
                     <h5 class="mb-4">Gain</h5>
 
 
-                    <div class="d-flex justify-content-center gap-5 mb-4">
+                    <div class="knob-grid mb-4">
 
-                        <div class="knob-container text-center">
+                        <div class="knob-container">
                             <div class="knob" id="gainKnob" data-value-id="gainValue" data-min="0.5" data-max="100" data-start="50" data-step="1">
                                 <div class="knob-indicator"></div>
                             </div>
@@ -26,8 +26,8 @@ include 'includes/header.php';
                             </div>
                         </div>
 
-                        <div class="knob-container text-center">
-                            <div class="knob" id="toneKnob" data-value-id="toneValue" data-min="0.5" data-max="1" data-start="0.5" data-step="0.01">
+                        <div class="knob-container">
+                            <div class="knob" id="toneKnob" data-value-id="toneValue" data-min="0" data-max="1" data-start="0.5" data-step="0.01">
                                 <div class="knob-indicator"></div>
                             </div>
                             <div class="mt-2">
@@ -38,38 +38,131 @@ include 'includes/header.php';
 
                     </div>
 
-                    <label class="led-switch mb-0">
-                        <input type="checkbox" class="effect-toggle"> <!--For toggle functionality-->
+                    <label class="led-switch mb-4">
+                        <input type="checkbox" id="gainSwitch" class="effect-toggle"> <!--For toggle functionality-->
                         <span class="led"></span> <!--Styled as a circle LED-->
                     </label>
                 </div>
 
                 <div class="border effect-panel">
-                    <h5>Tremolo</h5>
-                    <label class="led-switch mb-0">
-                        <input type="checkbox" class="effect-toggle"> <!--For toggle functionality-->
+                    <h5 class="mb-4">Tremolo</h5>
+
+                    <div class="knob-grid mb-4">
+
+                        <div class="knob-container">
+                            <div class="knob" id="tremoloFrequencyKnob" data-value-id="tremoloFrequencyValue" data-min="0.1" data-max="20" data-start="5" data-step="0.1">
+                                <div class="knob-indicator"></div>
+                            </div>
+                            <div class="mt-2">
+                                <div><b>Frequency</b></div>
+                                <div id="tremoloFrequencyValue">5</div>
+                            </div>
+                        </div>
+
+                        <div class="knob-container">
+                            <div class="knob" id="depthKnob" data-value-id="depthValue" data-min="0" data-max="1" data-start="0.5" data-step="0.01">
+                                <div class="knob-indicator"></div>
+                            </div>
+                            <div class="mt-2">
+                                <div><b>Depth</b></div>
+                                <div id="depthValue">0.1</div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <label class="led-switch mb-4">
+                        <input type="checkbox" id="tremoloSwitch" class="effect-toggle"> <!--For toggle functionality-->
                         <span class="led"></span> <!--Styled as a circle LED-->
                     </label>
                 </div>
 
                 <div class="border effect-panel">
-                    <h5>Chorus</h5>
-                    <label class="led-switch mb-0">
-                        <input type="checkbox" class="effect-toggle"> <!--For toggle functionality-->
+                    <h5 class="mb-4">Chorus</h5>
+
+                    <div class="knob-grid mb-4">
+
+                        <div class="knob-container">
+                            <div class="knob" id="mixKnob" data-value-id="mixValue" data-min="0" data-max="1" data-start="0.5" data-step="0.01">
+                                <div class="knob-indicator"></div>
+                            </div>
+                            <div class="mt-2">
+                                <div><b>Mix</b></div>
+                                <div id="mixValue">0.5</div>
+                            </div>
+                        </div>
+
+                        <div class="knob-container">
+                            <div class="knob" id="amplitudeKnob" data-value-id="amplitudeValue" data-min="0" data-max="10" data-start="3" data-step="0.1">
+                                <div class="knob-indicator"></div>
+                            </div>
+                            <div class="mt-2">
+                                <div><b>Amplitude (ms)</b></div>
+                                <div id="amplitudeValue">3</div>
+                            </div>
+                        </div>
+                        <div class="knob-container">
+                            <div class="knob" id="chorusFrequencyKnob" data-value-id="chorusFrequencyValue" data-min="0.1" data-max="5" data-start="0.5" data-step="0.1">
+                                <div class="knob-indicator"></div>
+                            </div>
+                            <div class="mt-2">
+                                <div><b>Frequency</b></div>
+                                <div id="chorusFrequencyValue">0.5</div>
+                            </div>
+                        </div>
+
+                        <div class="knob-container">
+                            <div class="knob" id="avgdelayKnob" data-value-id="avgdelayValue" data-min="5" data-max="30" data-start="20" data-step="1">
+                                <div class="knob-indicator"></div>
+                            </div>
+                            <div class="mt-2">
+                                <div><b>Avg Delay (ms)</b></div>
+                                <div id="avgdelayValue">20</div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <label class="led-switch mb-4">
+                        <input type="checkbox" id="chorusSwitch" class="effect-toggle"> <!--For toggle functionality-->
                         <span class="led"></span> <!--Styled as a circle LED-->
                     </label>
                 </div>
 
                 <div class="border effect-panel">
-                    <h5>Delay</h5>
-                    <label class="led-switch mb-0">
-                        <input type="checkbox" class="effect-toggle"> <!--For toggle functionality-->
+                    <h5 class="mb-4">Delay</h5>
+
+                    <div class="knob-grid mb-4">
+                        <div class="knob-container">
+                            <div class="knob" id="delayKnob" data-value-id="delayValue" data-min="10" data-max="800" data-start="100" data-step="1">
+                                <div class="knob-indicator"></div>
+                            </div>
+                            <div class="mt-2">
+                                <div><b>Delay (ms)</b></div>
+                                <div id="delayValue">80</div>
+                            </div>
+                        </div>
+
+                        <div class="knob-container">
+                            <div class="knob" id="feedbackKnob" data-value-id="feedbackValue" data-min="0" data-max="1" data-start="0.5" data-step="0.01">
+                                <div class="knob-indicator"></div>
+                            </div>
+                            <div class="mt-2">
+                                <div><b>Feedback</b></div>
+                                <div id="feedbackValue">0.5</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <label class="led-switch mb-4">
+                        <input type="checkbox" id="delaySwitch" class="effect-toggle"> <!--For toggle functionality-->
                         <span class="led"></span> <!--Styled as a circle LED-->
                     </label>
                 </div>
             </div>
-            <div>
-                <p>stuff</p>
+            <div class="d-flex flex-row align-items-center justify-content-center gap-3">
+                <audio id="demoPlayer" src="/audio/guitar_sample.wav" controls></audio>
+                <button id="generateButton" type="button" class="btn btn-success">Generate</button>
             </div>
         </div>
     </div>
--- js/effect-interface.js
+++ js/effect-interface.js
@@ -1,52 +1,328 @@
-function initializeKnob(knob) {
-    const valueId = knob.dataset.valueId;
-    const valueDisplay = document.getElementById(valueId);
+const sampleRate = 44100;
+const samplePath = "/audio/guitar_sample.wav";
+const gainKnob = document.getElementById("gainKnob");
+const toneKnob = document.getElementById("toneKnob");
+const tremoloFrequencyKnob = document.getElementById("tremoloFrequencyKnob");
+const depthKnob = document.getElementById("depthKnob");
+const mixKnob = document.getElementById("mixKnob");
+const amplitudeKnob = document.getElementById("amplitudeKnob");
+const chorusFrequencyKnob = document.getElementById("chorusFrequencyKnob");
+const avgdelayKnob = document.getElementById("avgdelayKnob");
+const delayKnob = document.getElementById("delayKnob");
+const feedbackKnob = document.getElementById("feedbackKnob");
 
-    const min = Number(knob.dataset.min ?? 0);
-    const max = Number(knob.dataset.max ?? 100);
-    const step = Number(knob.dataset.step ?? 1);
-    let value = Number(knob.dataset.start ?? min);
+class WavData {
+  constructor() {
+    this.chunkID = "";
+    this.chunkSize = 0;
+    this.format = "";
 
-    let isDragging = false;
+    this.subChunk1ID = "";
+    this.subChunk1Size = 0;
+    this.audioFormat = 0;
+    this.numChannels = 0;
+    this.sampleRate = 0;
+    this.byteRate = 0;
+    this.blockAlign = 0;
+    this.bitsPerSample = 0;
 
-    function setKnobRotation(val) {
-        const minAngle = -135;
-        const maxAngle = 135;
-        const percent = (val - min) / (max - min);
-        const angle = minAngle + percent * (maxAngle - minAngle);
+    this.subChunk2ID = "";
+    this.subChunk2Size = 0;
+    this.samples = new Int16Array();
+  }
 
-        knob.style.transform = `rotate(${angle}deg)`;
+  static readString(view, offset, length) {
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += String.fromCharCode(view.getUint8(offset + i));
+    }
+    return result;
+  }
 
-        if (step < 1) {
-            const decimals = (step.toString().split(".")[1] || "").length;
-            valueDisplay.textContent = val.toFixed(decimals);
-        } else {
-            valueDisplay.textContent = Math.round(val);
-        }
+  static writeString(view, offset, str) {
+    for (let i = 0; i < str.length; i++) {
+      view.setUint8(offset + i, str.charCodeAt(i));
+    }
+  }
+
+  static fromArrayBuffer(buffer) {
+    const wav = new WavData();
+    const view = new DataView(buffer);
+
+    wav.chunkID = WavData.readString(view, 0, 4);
+    wav.chunkSize = view.getUint32(4, true);
+    wav.format = WavData.readString(view, 8, 4);
+
+    wav.subChunk1ID = WavData.readString(view, 12, 4);
+    wav.subChunk1Size = view.getUint32(16, true);
+    wav.audioFormat = view.getUint16(20, true);
+    wav.numChannels = view.getUint16(22, true);
+    wav.sampleRate = view.getUint32(24, true);
+    wav.byteRate = view.getUint32(28, true);
+    wav.blockAlign = view.getUint16(32, true);
+    wav.bitsPerSample = view.getUint16(34, true);
+
+    wav.subChunk2ID = WavData.readString(view, 36, 4);
+    wav.subChunk2Size = view.getUint32(40, true);
+
+    const sampleCount = wav.subChunk2Size / 2;
+    wav.samples = new Int16Array(sampleCount);
+
+    let sampleOffset = 44;
+    for (let i = 0; i < sampleCount; i++) {
+      wav.samples[i] = view.getInt16(sampleOffset, true);
+      sampleOffset += 2;
     }
 
-    knob.addEventListener("mousedown", (e) => {
-        e.preventDefault();
-        isDragging = true;
-        document.body.classList.add("no-select");
-    });
+    return wav;
+  }
 
-    document.addEventListener("mouseup", () => {
-        isDragging = false;
-        document.body.classList.remove("no-select");
-    });
+  toArrayBuffer() {
+    const buffer = new ArrayBuffer(44 + this.subChunk2Size);
+    const view = new DataView(buffer);
 
-    document.addEventListener("mousemove", (e) => {
-        if (!isDragging) return;
+    WavData.writeString(view, 0, this.chunkID);
+    view.setUint32(4, this.chunkSize, true);
+    WavData.writeString(view, 8, this.format);
 
-        const sensitivity = (max - min) / 200;
-        value += e.movementX * sensitivity;
-        value = Math.max(min, Math.min(max, value)); //clamp
+    WavData.writeString(view, 12, this.subChunk1ID);
+    view.setUint32(16, this.subChunk1Size, true);
+    view.setUint16(20, this.audioFormat, true);
+    view.setUint16(22, this.numChannels, true);
+    view.setUint32(24, this.sampleRate, true);
+    view.setUint32(28, this.byteRate, true);
+    view.setUint16(32, this.blockAlign, true);
+    view.setUint16(34, this.bitsPerSample, true);
 
-        setKnobRotation(value);
-    });
+    WavData.writeString(view, 36, this.subChunk2ID);
+    view.setUint32(40, this.subChunk2Size, true);
 
-    setKnobRotation(value);
+    let sampleOffset = 44;
+    for (let i = 0; i < this.samples.length; i++) {
+      view.setInt16(sampleOffset, this.samples[i], true);
+      sampleOffset += 2;
+    }
+
+    return buffer;
+  }
 }
 
+function initializeKnob(knob) {
+  const valueId = knob.dataset.valueId;
+  const valueDisplay = document.getElementById(valueId);
+
+  const min = Number(knob.dataset.min ?? 0);
+  const max = Number(knob.dataset.max ?? 100);
+  const step = Number(knob.dataset.step ?? 1);
+  let value = Number(knob.dataset.start ?? min);
+
+  let isDragging = false;
+
+  function setKnobRotation(val) {
+    const minAngle = -135;
+    const maxAngle = 135;
+    const percent = (val - min) / (max - min);
+    const angle = minAngle + percent * (maxAngle - minAngle);
+
+    knob.style.transform = `rotate(${angle}deg)`;
+
+    if (step < 1) {
+      const decimals = (step.toString().split(".")[1] || "").length;
+      valueDisplay.textContent = val.toFixed(decimals);
+    } else {
+      valueDisplay.textContent = Math.round(val);
+    }
+  }
+
+  knob.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    isDragging = true;
+    document.body.classList.add("no-select");
+  });
+
+  document.addEventListener("mouseup", () => {
+    isDragging = false;
+    document.body.classList.remove("no-select");
+  });
+
+  document.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+
+    const sensitivity = (max - min) / 200;
+    value += e.movementX * sensitivity;
+    value = Math.max(min, Math.min(max, value)); //clamp
+
+    setKnobRotation(value);
+  });
+
+  setKnobRotation(value);
+}
+
+function applyGain(samples) {
+  const gain = parseFloat(
+    document.getElementById(gainKnob.dataset.valueId).textContent,
+  );
+  const tone = parseFloat(
+    document.getElementById(toneKnob.dataset.valueId).textContent,
+  );
+
+  const MAX = 32767;
+
+  for (let i = 0; i < samples.length; i++) {
+    let x = (samples[i] * gain) / 32768;
+
+    if (x >= 1) {
+      samples[i] = MAX;
+    } else if (x <= -1) {
+      samples[i] = -MAX;
+    } else {
+      let y = x - (x * x * x) / 3;
+      samples[i] = y * MAX;
+    }
+
+    //tone
+    if (i != 0) {
+      samples[i] = tone * samples[i] + (1 - tone) * samples[i - 1];
+    } else {
+      samples[i] = tone * samples[i];
+    }
+  }
+}
+
+function applyTremolo(samples) {
+  const depth = parseFloat(
+    document.getElementById(depthKnob.dataset.valueId).textContent,
+  );
+  const frequency = parseFloat(
+    document.getElementById(tremoloFrequencyKnob.dataset.valueId).textContent,
+  );
+  const amplitude = depth * 0.5;
+
+  console.log(depth);
+  console.log(frequency);
+
+  for (let i = 0; i < samples.length; i++) {
+    let wave = Math.sin((2 * Math.PI * frequency * i) / sampleRate);
+    samples[i] = Math.floor((amplitude * wave + 1 - amplitude) * samples[i]);
+  }
+}
+
+function applyChorus(samples) {
+  const mix = parseFloat(
+    document.getElementById(mixKnob.dataset.valueId).textContent,
+  );
+  const amplitude = parseFloat(
+    document.getElementById(amplitudeKnob.dataset.valueId).textContent,
+  );
+  const frequency = parseFloat(
+    document.getElementById(chorusFrequencyKnob.dataset.valueId).textContent,
+  );
+  const avgdelay = parseFloat(
+    document.getElementById(avgdelayKnob.dataset.valueId).textContent,
+  );
+
+  const d0 = (avgdelay / 1000) * sampleRate;
+  const a = (amplitude / 1000) * sampleRate;
+  const w = 2 * Math.PI * frequency;
+
+  let output = new Int16Array(samples);
+
+  for (let i = 0; i < samples.length; i++) {
+    const dn = d0 + a * Math.sin((w * i) / sampleRate);
+
+    const sample0 = Math.floor(dn);
+    const sample1 = sample0 + 1;
+    const frac = dn - sample0;
+
+    if (i - sample1 < 0) {
+      //DN sample does not exist
+      output[i] = samples[i];
+      continue;
+    } else {
+      const interpSample =
+        (1 - frac) * samples[i - sample0] + frac * samples[i - sample1];
+      let buffer = (1 - mix) * samples[i] + mix * interpSample;
+
+      if (buffer < -32767) {
+        buffer = -32767;
+      }
+      if (buffer > 32767) {
+        buffer = 32767;
+      }
+
+      output[i] = buffer;
+    }
+  }
+
+  return output;
+}
+
+function applyDelay(samples) {
+  const delay = parseFloat(
+    document.getElementById(delayKnob.dataset.valueId).textContent,
+  );
+  const feedback = parseFloat(
+    document.getElementById(feedbackKnob.dataset.valueId).textContent,
+  );
+
+  const d = Math.floor((delay / 1000) * sampleRate);
+
+  for (let i = 0; i < samples.length; i++) {
+    if (i - d < 0) continue; //sample does not exist
+
+    let buffer = feedback * samples[i - d];
+    if (buffer < -32767) {
+      buffer = -32767;
+    }
+    if (buffer > 32767) {
+      buffer = 32767;
+    }
+
+    samples[i] += buffer;
+  }
+}
+
+async function GenerateAudio() {
+  //Check which effects are currently active
+  const gainEnabled = document.getElementById("gainSwitch").checked;
+  const tremoloEnabled = document.getElementById("tremoloSwitch").checked;
+  const chorusEnabled = document.getElementById("chorusSwitch").checked;
+  const delayEnabled = document.getElementById("delaySwitch").checked;
+
+  //Retrieve samples
+  const file = await fetch(samplePath);
+  const buffer = await file.arrayBuffer();
+  const data = WavData.fromArrayBuffer(buffer);
+
+  //Modify samples
+  if (gainEnabled) {
+    applyGain(data.samples);
+  }
+
+  if (tremoloEnabled) {
+    applyTremolo(data.samples);
+  }
+
+  if (chorusEnabled) {
+    data.samples = applyChorus(data.samples);
+  }
+
+  if (delayEnabled) {
+    applyDelay(data.samples);
+  }
+
+  //Set the audio player to use the new samples
+  const outputBuffer = data.toArrayBuffer();
+  const blob = new Blob([outputBuffer], { type: "audio/wav" });
+  const url = URL.createObjectURL(blob);
+
+  const audioPlayer = document.getElementById("demoPlayer");
+  audioPlayer.src = url;
+  audioPlayer.load();
+
+}
+
+document
+  .getElementById("generateButton")
+  .addEventListener("click", GenerateAudio);
 document.querySelectorAll(".knob").forEach(initializeKnob);
